1dbe66353f7622305f8bbd1f7a7b0d225943c7cd
CodeBro <daoquyen833@gmail.com>
2022-05-12 15:50:02 +0700

    update file Exercises09

--- hard/Exercises09.js
+++ hard/Exercises09.js
@@ -1,18 +1,18 @@
-function search_word(text, word){    
+function search_word(text, word) {    
     var x = 0, y=0;   
-    for (i=0;i< text.length;i++) {
+    for (i = 0; i < text.length; i++) {
         if(text[i] == word[0]) {
-            for(j=i;j< i+word.length;j++) {
-                if(text[j]==word[j-i]) {
+            for(j = i; j < i + word.length; j++) {
+                if(text[j] == word[j - i]) {
                     y++;
-                  }
-                if (y==word.length) {
+                }
+                if (y == word.length) {
                   x++;
                 }
             }
-            y=0;
+            y = 0;
         }
     }
-   return "'"+word+"' xuat hien " + x + " lan.";
+    return "'" + word + "' xuat hien " + x + " lan.";
 }
 console.log(search_word('that su hom nay em rat muon nghi nhung em khong co tien nen em lai di lam kiem tien', 'em'));
